ced37720c9fb1cd1ec89bcc429f6f606a5bf504d
Marcos Vinícius Rodrigues Ramos <mvrr1232@gmail.com>
2020-03-24 01:23:46 -0300

    Delete ex002.py

--- ex002.py
+++
@@ -1,4 +0,0 @@
-nome = input('Digite seu nome:')
-# .format(variavel), faz com que a variavel, no caso nome
-# Seja formatada para caber dentro das {}
-print ('É um prazer te conhecer,{}!'.format(nome))
